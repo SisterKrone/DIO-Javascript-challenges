63fb97da851b4bfbed06e6cb2f58cde67b308696
SisterKrone <luiz.fabiana12@gmail.com>
2025-09-15 22:10:11 -0300

    Add Pokedex

--- modulo2/pokedex/script.js
+++
@@ -1,7 +0,0 @@
-(function main() {
-    const btnTeste = document.getElementById('btnTeste');
-    console.log(btnTeste);
-    btnTeste.addEventListener('click',()=>{
-        console.log('Clickou no botao');
-    });
-})();
