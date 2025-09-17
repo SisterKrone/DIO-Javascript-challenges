515eeb892c24712ba960346a02893cc112136a8d
SisterKrone <luiz.fabiana12@gmail.com>
2025-09-17 11:27:15 -0300

    Add pokemon img from poke api

--- modulo2/pokedex/assets/js/main.js
+++ modulo2/pokedex/assets/js/main.js
@@ -1,21 +1,20 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
 (function main() {
+
+    function convertPokemonTypesToLi(pokemonTypes) {
+        return pokemonTypes.map((typeSlot) =>{return `<li class="type">${typeSlot.type.name}</li>`})
+    }
 
     function convertPokemonToLi(pokemon) {
         return `
             <li class="pokemon">
-                <span class="number">#001</span>
+                <span class="number">#${pokemon.order}</span>
                 <span class="name">${pokemon.name}</span>
                 
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">Grass</li>
-                        <li class="type">Poison</li>
+                       ${convertPokemonTypesToLi(pokemon.types).join('')}
                     </ol>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                    <img src="${pokemon.sprites.other.dream_world.front_default}"
                         alt="${pokemon.name}">
                 </div>
             </li>
@@ -25,14 +24,11 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
     const pokemonList = document.getElementById('pokemonList');
 
-    pokeApi.getPokemons().then((pokemons) => {
-        const listItems = []
+    pokeApi.getPokemons().then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('');
+        pokemonList.innerHTML = newHtml
 
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i]; 
-            listItems.push(convertPokemonToLi(pokemon));           
-        }
-        console.log(listItems)
-    })       
+    })
+
 
 })();
