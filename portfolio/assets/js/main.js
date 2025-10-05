afcef98a2bd31064cb4d7f7cfb5da44d44422d12
SisterKrone <luiz.fabiana12@gmail.com>
2025-10-05 20:35:08 -0300

    Add softSkills

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -1,11 +1,36 @@
-function updateProfileInfo(profileData){
+function updateProfileInfo(profileData) {
+    console.log(profileData)
     const photo = document.getElementById('profile.photo')
     photo.src = profileData.photo
     photo.alt = profileData.name
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+    job.href = profileData.linkedin
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+    phone.href = `tel:${profileData.phone}`
+
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+    email.herf = `mailto:${profileData.email}`
+
 }
 
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill=>`<li>${skill}</li>`).join('')
+}
 
 (async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
+    updateSoftSkills(profileData)
 })()
